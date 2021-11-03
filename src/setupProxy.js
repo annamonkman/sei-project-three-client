2186c87a99228a32f47bb77ec78448e7722a6c63
Anna Monkman <annamonkman@me.com>
2021-11-03 11:00:27 +0000

    fix api target setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,5 +2,5 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 // import { createProxyMiddleware } from 'http-proxy-middleware'
 
 module.exports = function (app) {
-  app.use(createProxyMiddleware('/api', { target: 'https://festivalist-apiherokuapp.com', "changeOrigin": true }))
+  app.use(createProxyMiddleware('/api', { target: 'https://festivalist-api.herokuapp.com', "changeOrigin": true }))
 }
